d0be3101439642535c65f62dea966858cfa0b663
VIJAY32565 <perabattulavijay9.9.p@gmail.com>
2026-04-30 10:32:31 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,73 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a=6
+b=4
+print("sum is:",a+b)
+sum is: 10
+print("substraction is :",a-b)
+substraction is : 2
+print("multiplication is :",a*b)
+multiplication is : 24
+print("modlus is :",a%b)
+modlus is : 2
+print("floor division is:",a//b)
+floor division is: 1
+print("exponent is:",a**b)
+exponent is: 1296
+print("division is :",a/b)
+division is : 1.5
+print(a>b)
+True
+print(a<b)
+False
+print(>=b)
+SyntaxError: invalid syntax
+printprint(>=b)
+SyntaxError: invalid syntax
+print(a>=b)
+True
+print(a<=b)
+False
+print(a==)
+SyntaxError: invalid syntax
+print(a==b)
+False
+print(a!=b)
+True
+a=5
+b=7
+print(a,b)
+5 7
+a+=b
+print(a,b)
+12 7
+a=True
+b=False
+print(a and b)
+False
+>>> print(a or b)
+True
+>>> print(not a)
+False
+>>> print(not b)
+True
+>>> a=7
+>>> b=5
+>>> print(a & b)
+5
+>>> print(a ! b)
+SyntaxError: invalid syntax
+>>> print(a|)
+SyntaxError: invalid syntax
+>>> print(a|b)
+7
+>>> print(a * b)
+35
+>>> print(~a)
+-8
+>>> print(a<<2)
+28
+>>> print(a>>b)
+0
+>>> print(a>>2)
+1
